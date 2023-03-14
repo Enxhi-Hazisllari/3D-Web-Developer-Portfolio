81ab70578c1660170cc049c78de08dd7295e6b65
EnxhiHazisllari <enxhi.hazisllari14@gmail.com>
2023-03-14 14:00:32 +0100

    Update CanvasLoader

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,6 +1,7 @@
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
+import CanvasLoader from "../CanvasLoader ";
 
 const Computers = () => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
@@ -27,7 +28,7 @@ const Computers = () => {
 const ComputersCanvas = () => {
   return(
     <Canvas frameloop="demand" shadows camera={{position: [20, 3, 5], fov: 25}} gl={{preserveDrawingBuffer: true}}>
-      <Suspense>
+      <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2}/>
         <Computers />
       </Suspense> 
