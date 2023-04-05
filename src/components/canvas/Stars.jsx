b6e637eb72c5117f970e3c1b944b2ef58121f12d
Enxhi-Hazisllari <enxhi.hazisllari14@gmail.com>
2023-04-05 14:24:37 +0200

    Fix Stars

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,11 +1,19 @@
 import { useState, useRef, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
-import * as random from "maath/random/dist/maath-random.esm";
 
 const Stars = (props) => {
   const ref = useRef();
-  const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }));
+
+  const [positions] = useState(() => {
+    const array = new Float32Array(5000 * 3);
+
+    for (let i = 0; i < array.length; i++) {
+      array[i] = (Math.random() - 0.5) * 2.4;
+    }
+
+    return array;
+  });
 
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 10;
@@ -14,7 +22,7 @@ const Stars = (props) => {
 
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
-      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+      <Points ref={ref} positions={positions} stride={3} frustumCulled {...props}>
         <PointMaterial
           transparent
           color='#f272c8'
